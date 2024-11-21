cdb873b09c0e3de280175032f9a8ac2d74030f9c
noe1312 <matiaz1909@gmail.com>
2024-11-21 13:19:42 -0300

    Sexto Tutorial

--- polls/views.py
+++ polls/views.py
@@ -12,18 +12,19 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
         :5
-    ]
+        ]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
